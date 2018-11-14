b66d9def91312c60083aa9e7bc1469c69dbbccb5
Graham Williams <graham.williams@togaware.com>
2018-11-14 15:00:11 +0800

    Synchronise with mlhub standards.

--- configure.R
+++ configure.R
@@ -1,45 +1,54 @@
-# Ubuntu system dependencies for the pre-built model. We choose to
-# install user local packages rather than OS supplied packages to
-# avoid the need for sys admin access from within mlhub. R is
-# often operating system installed.
+# Install and configure Ubuntu system dependencies for the pre-built model.
+#
+# We choose to install user local packages using install-packages()
+# rather than OS supplied packages to minimise the need for sys admin
+# access from within mlhub. R itself is often operating system
+# installed though not necessarily.
 
-# Identify the required packages.
+# Identify the required R packages for this model.
 
 packages <- c("ggplot2", "dplyr", "caret", "plyr")
 
-# Determine which need to be installed.
+# Determine which packages need to be installed.
 
 install  <- packages[!(packages %in% installed.packages()[,"Package"])]
 
-# Identify where they will be installed - the user's local R library.
+# Report on what is already installed.
+
+already <- setdiff(packages, install)
+if (length(already))
+{
+    cat("The following required R packages are already installed:\n",
+        paste(already, collapse=" "))
+}
+
+# Install into the package local R library.
 
 lib <- file.path("./R")
 
-# Ensure the user's local R library exists.
+# Ensure the library exists.
 
 dir.create(lib, showWarnings=FALSE, recursive=TRUE)
 
-# Install the packages into the local R library.
+# Install packages into the local R library.
 
 if (length(install))
 {
-  cat(sprintf("\nInstalling '%s' into '%s'...", paste(install, collapse="', '"), lib))
+  cat(sprintf("Installing '%s' into '%s'...", paste(install, collapse="', '"), lib))
   install.packages(install, lib=lib)
-} else
-{
-  cat("\nNo additional generic R packages need to be installed.")
 }
-cat("\n\n")
+cat("\n")
 
 # Additional specific packages, often as an interim measure.
 
-cat("We also need to install these specific packages...\n")
-
-pkgs <- c("SAR_0.0.1.tar.gz")
-for (pkg in pkgs)
+if (TRUE)
 {
-  cat("  ", pkg, "\n")
-  install.packages(pkg, repos=NULL, lib=lib)
-}
+  cat("\nWe also need to install these specific package versions...\n")
 
-cat("\n")
+  pkgs <- c("SAR_0.0.1.tar.gz")
+  for (pkg in pkgs)
+  {
+    cat("", basename(pkg), "\n")
+    install.packages(pkg, repos=NULL, lib=lib)
+  }
+}
