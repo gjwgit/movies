cbe9fbf89a81e740150abf2c5a14c92ca859373d
Graham Williams <graham.williams@togaware.com>
2018-12-22 16:24:45 +0800

    Update messaging.

--- configure.R
+++ configure.R
@@ -6,7 +6,7 @@ lib <- file.path("./R")
 
 dir.create(lib, showWarnings=FALSE, recursive=TRUE)
 
-cat("\n*** We also need to install this local package versions...\n")
+cat("*** Install local packages ...\n")
 
 pkgs <- c("SAR_0.0.1.tar.gz")
 for (pkg in pkgs)
