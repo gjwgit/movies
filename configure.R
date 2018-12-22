b5836c6963c4c4e69ced7a6162dcd4a08e565c0a
Graham Williams <graham.williams@togaware.com>
2018-12-22 16:21:27 +0800

    Setup lib for R install.

--- configure.R
+++ configure.R
@@ -1,3 +1,11 @@
+# Install into the package local R library.
+
+lib <- file.path("./R")
+
+# Ensure the library exists.
+
+dir.create(lib, showWarnings=FALSE, recursive=TRUE)
+
 cat("\n*** We also need to install this local package versions...\n")
 
 pkgs <- c("SAR_0.0.1.tar.gz")
